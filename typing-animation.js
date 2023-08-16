f511ec535017db2ce6cc409a03952cdd9fc363b6
neilmhta <neilmhta@gmail.com>
2023-08-16 16:57:09 -0400

    updated blackout animation

--- typing-animation.js
+++ typing-animation.js
@@ -18,7 +18,7 @@
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
-        var delta = 100 - Math.random() * 50;
+        var delta = 45 - Math.random() * 15;
 
         if (this.isDeleting) { delta /= 2; }
 
@@ -38,18 +38,29 @@
         }, delta);
     };
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
-        document.body.appendChild(css);
-    };
+    }
+
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+    document.body.appendChild(css);
+
+    // Change body and text color at the end of transition
+    var lastElement = elements[elements.length - 1]; // Assuming the last element triggers the transition
+    var fullTxt = lastElement.getAttribute('data-type');
+
+    setTimeout(function() {
+        document.body.style.backgroundColor = '#eef2e4'; // Replace 'new-color' with the desired background color
+        document.body.style.color = 'black';     // Replace 'new-text-color' with the desired text color
+
+    }, fullTxt.length * 45); // Adjust the delay based on your transition timings
+};
